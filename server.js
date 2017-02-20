b813d9baa35cc4a8a9c6bef265e50610785fc75f
Rodrigo Alves <rodrigo.alves@cesar.org.br>
2017-02-20 16:34:37 -0300

    Listen to dbus signals

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var dbus = require('dbus-native');
 
 var config = require('./app/config');
 var apiRoute = require('./app/routes/api');
@@ -7,6 +8,11 @@ var handlers = require('./app/helpers/handlers');
 
 var publicRoot = __dirname + '/public/'; // eslint-disable-line no-path-concat
 var serverConfig = express();
+var sessionBus = dbus.sessionBus();
+
+var targetServiceName = 'org.gtk.GDBus.TestServer';
+var targetIfaceName = 'org.gtk.GDBus.TestInterface';
+var targetObjectPath = '/org/gtk/GDBus/TestObject';
 
 serverConfig.use(bodyParser.json());
 serverConfig.use(bodyParser.urlencoded({ extended: true }));
@@ -19,3 +25,19 @@ serverConfig.use(handlers.errorHandler);
 serverConfig.listen(config.PORT, function () {
   console.log('Listening on ' + config.PORT);
 });
+
+if (!sessionBus) {
+  throw new Error('Could not connect to the DBus session bus.');
+}
+
+sessionBus.getService(targetServiceName).getInterface(
+  targetObjectPath, targetIfaceName, function (err, notifications) {
+    if (err || !notifications) {
+      console.error('Could not query interface \'' + targetIfaceName + '\', the error was: ' + err ? err : '(no error)');
+      process.exit(1);
+    }
+    console.log('Listening to nrfd signals');
+    notifications.on('VelocityChanged', function () {
+      console.log('VelocityChanged', arguments);
+    });
+  });
